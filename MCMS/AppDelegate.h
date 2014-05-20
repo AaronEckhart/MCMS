a24371eb336507f6f183888d61958e702df34024
Billy Everyteen <aeckh002@fiu.edu>
2014-05-20 13:39:06 -0500

    initial commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by AE on 5/20/14.
+//  Copyright (c) 2014 Aaron Eckhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MCMS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by AE on 5/20/14.
+//  Copyright (c) 2014 Aaron Eckhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
